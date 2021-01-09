f97a69354f8c9aa6b5361fa6457c88a7e7fd8a51
Naresh <naresh1934@gmail.com>
2021-01-08 20:31:51 -0600

    added branch in Main.java

--- Banking/src/com/company/Main.java
+++ Banking/src/com/company/Main.java
@@ -5,26 +5,26 @@ public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank("Bank of Canada");
         bank.addBranch("Montreal");
-        bank.addCustomer("Montreal","Naresh",500.50);
-        bank.addCustomer("Montreal","Juhi",1000.02);
+        bank.addCustomer("Montreal","Tom",500.50);
+        bank.addCustomer("Montreal","Harry",1000.02);
 
         bank.addBranch("Toronto");
-        bank.addCustomer("Toronto","JB",500.50);
-        bank.addCustomer("Toronto","Atharv",1000.02);
+        bank.addCustomer("Toronto","Bob",500.50);
+        bank.addCustomer("Toronto","Jack",1000.02);
 
-        bank.addCustomerTransaction("Montreal","Naresh",200.20);
-        bank.addCustomerTransaction("Montreal","Juhi",350.20);
-        bank.addCustomerTransaction("Montreal","Naresh",600.20);
+        bank.addCustomerTransaction("Montreal","Tom",200.20);
+        bank.addCustomerTransaction("Montreal","Harry",350.20);
+        bank.addCustomerTransaction("Montreal","Bob",600.20);
 
         bank.listCustomer("Montreal",true);
         bank.listCustomer("Toronto",true);
 
-        bank.addBranch("Winnipeg");
+        bank.addBranch("Ottawa");
 
-        if(!bank.addCustomer("Winnipeg","Jignesh",300.00)){
+        if(!bank.addCustomer("Ottawa","Mike",300.00)){
             System.out.println("Branch does not exists");
         }
-        bank.listCustomer("Winnipeg",true);
+        bank.listCustomer("Ottawa",true);
 
         if(!bank.addBranch("Montreal")){
             System.out.println("Branch already exists");
